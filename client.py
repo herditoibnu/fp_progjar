923ed988eb4f10ba9cfb977c95984967745a03e8
herditoibnu <herditoibnu@gmail.com>
2016-12-15 08:50:13 +0700

    client bisa request path sama nerima file, tapi masih teks doang nerimanya

--- client.py
+++ client.py
@@ -2,14 +2,19 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server_address = ('10.181.1.201', 8000)
-server_address = ('localhost', 8000)
+server_address = ('localhost', 50001)
 client_socket.connect(server_address)
 
 #request_header = 'GET / HTTP/1.1\r\nHost: 10.181.1.201\r\n\r\n'
-request_header = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n'
-client_socket.send(request_header)
+# request_header = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n'
+request_header_type = 'GET /'
+request_header_path = raw_input('input path: ')
+request_header_http = ' HTTP/1.1\r\nHost: localhost\r\n\r\n'
+request_header_all = request_header_type + request_header_path + request_header_http
+
+client_socket.send(request_header_all)
 
 response = ''
-recv = client_socket.recv(1024)
+recv = client_socket.recv(10240)
 print recv
 client_socket.close()
